9f6295fe064d412577d30694702d2cc3d6c332ed
Aman Dangol <amandangol423@gmail.com>
2023-06-25 13:31:20 +0545

    detects if there are any playable space left

--- tictactoe.js
+++ tictactoe.js
@@ -1,23 +1,58 @@
 let ele="";
 let playerCount=0;
+let i="";
+let check=true;
 function clicked(index){
-    let i =index;
+     i =index;
     ele=document.getElementById(i);
-    if(playerCount%2==0){
-        if(ele.style.backgroundColor!="red"){
-            ele.style.backgroundColor="red";
-            playerCount++;
-        }else{
-            console.log("not available");
+    if(check==true){
+        if(playerCount%2==0){
+            if(ele.style.backgroundColor!='blue'){
+                if(ele.style.backgroundColor!="red"){
+                    ele.style.backgroundColor="red";
+                    playerCount++;                    
+                }else{
+                    alert("youve already occupied this place");
+                }
+            }
+            else{
+                alert("the space is already occupied");
+            } 
+        }
+        else{
+            if(ele.style.backgroundColor!='red'){
+                if(ele.style.backgroundColor!="blue"){
+                    ele.style.backgroundColor="blue";
+                    playerCount++;
+                }else{
+                    alert("youve already occupied this place");
+                }
+            }
+            else{
+                alert("the space is already occupied");
+            }
+
+        }
+        spaceLeft();
+    }
+    
+
+}
+spaceLeft=()=>{
+    let count = 0;
+    for(let i=1;i<=9;i++){
+        if (document.getElementById(i).style.backgroundColor=='') {
+            count++;
+            check= true;
         }
     }
-    else{
-        if(ele.style.backgroundColor!="blue"){
-            ele.style.backgroundColor="blue";
-            playerCount++;
-        }else{
-            console.log("not available");
-        }
-    }
+    if (count==0) {
+        setTimeout(noSpace,100);
+        check=false;
+    }    
+
+}
+function noSpace(){
+    alert("no spce resart");
 }
 
