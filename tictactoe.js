c27a2a14638499eb6b6f660ae055f685231a4885
Aman Dangol <amandangol423@gmail.com>
2023-06-25 10:22:23 +0545

    added color to diiferentiate players

--- tictactoe.js
+++ tictactoe.js
@@ -1,6 +1,23 @@
 let ele="";
+let playerCount=0;
 function clicked(index){
     let i =index;
-    console.log("clicked from ",index);
-    ele=document.getElementById(i).style.backgroundColor="red";
+    ele=document.getElementById(i);
+    if(playerCount%2==0){
+        if(ele.style.backgroundColor!="red"){
+            ele.style.backgroundColor="red";
+            playerCount++;
+        }else{
+            console.log("not available");
+        }
+    }
+    else{
+        if(ele.style.backgroundColor!="blue"){
+            ele.style.backgroundColor="blue";
+            playerCount++;
+        }else{
+            console.log("not available");
+        }
+    }
 }
+
