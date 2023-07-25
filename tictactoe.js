19ab803586b2c4385e56eda6ee0c54a152701aba
Aman Dangol <amandangol423@gmail.com>
2023-07-25 12:43:28 +0545

    added one win condition

--- tictactoe.js
+++ tictactoe.js
@@ -2,57 +2,77 @@ let ele="";
 let playerCount=0;
 let i="";
 let check=true;
+let hasWon=false;
 function clicked(index){
      i =index;
     ele=document.getElementById(i);
-    if(check==true){
-        if(playerCount%2==0){
-            if(ele.style.backgroundColor!='blue'){
-                if(ele.style.backgroundColor!="red"){
-                    ele.style.backgroundColor="red";
-                    playerCount++;                    
-                }else{
-                    alert("youve already occupied this place");
+    if(hasWon==false){
+        if(check==true){
+            if(playerCount%2==0){
+                if(ele.style.backgroundColor!='blue'){
+                    if(ele.style.backgroundColor!="red"){
+                        ele.style.backgroundColor="red";
+                        setTimeout(() => {
+                            checkWin("red")
+                        }, 100);
+                        playerCount++;                    
+                    }else{
+                        alert("youve already occupied this place");
+                    }
                 }
+                else{
+                    alert("the space is already occupied");
+                } 
             }
             else{
-                alert("the space is already occupied");
-            } 
-        }
-        else{
-            if(ele.style.backgroundColor!='red'){
-                if(ele.style.backgroundColor!="blue"){
-                    ele.style.backgroundColor="blue";
-                    playerCount++;
-                }else{
-                    alert("youve already occupied this place");
+                if(ele.style.backgroundColor!='red'){
+                    if(ele.style.backgroundColor!="blue"){
+                        ele.style.backgroundColor="blue";
+                        playerCount++;
+                    }else{
+                        alert("youve already occupied this place");
+                    }
                 }
-            }
-            else{
-                alert("the space is already occupied");
-            }
-
-        }
-        spaceLeft();
-    }
+                else{
+                    alert("the space is already occupied");
+                }
     
-
+            }
+            spaceLeft();
+        } 
+    }   else{
+        alert("game has ended")
+    }
+       
 }
 spaceLeft=()=>{
     let count = 0;
-    for(let i=1;i<=9;i++){
-        if (document.getElementById(i).style.backgroundColor=='') {
-            count++;
-            check= true;
+    if(hasWon==false){
+        for(let i=1;i<=9;i++){
+            if (document.getElementById(i).style.backgroundColor=='') {
+                count++;
+                check= true;
+            }
         }
-    }
-    if (count==0) {
-        setTimeout(noSpace,100);
-        check=false;
-    }    
+        if (count==0) {
+            setTimeout(noSpace,100);
+            check=false;
+        } 
+    }  
+    else{
+        alert("game has ended ")
+    } 
 
 }
 function noSpace(){
-    alert("no spce resart");
+    alert("no space resart");
+}
+function checkWin(color){
+
+    if(document.getElementById(1).style.backgroundColor==color&&document.getElementById(4).style.backgroundColor==color && document.getElementById(7).style.backgroundColor==color){
+        alert(color+" wins");
+        hasWon=true;
+    }
+
 }
 
